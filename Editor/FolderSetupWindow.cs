91a63c112edb0a576bbfe44da878a4d8bd6829ce
jorobledodu <jorobledodu@gmail.com>
2025-10-22 18:06:46 +0200

    Update FolderSetupWindow.cs

--- Editor/FolderSetupWindow.cs
+++ Editor/FolderSetupWindow.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-namespace Jorobledodu.FolderSetup
+namespace jorobledodu.folderSetup
 {
     public class FolderSetupWindow : EditorWindow
     {
